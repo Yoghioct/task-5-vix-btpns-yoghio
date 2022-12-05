afa51b2557b2f86493358a9382fdf793467bef73
Yoghi o <yoghioct@gmail.com>
2022-12-05 14:41:34 +0700

    inital files - add package

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/login", authcontroller.Login).Methods("POST")
+	r.HandleFunc("/register", authcontroller.Register).Methods("POST")
+	r.HandleFunc("/logout", authcontroller.Logout).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
